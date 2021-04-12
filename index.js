b4acaf0a9e1bed6092914df82da5cdaeb8f86f13
tenzin333 <tenthinlay007@gmail.com>
2021-04-12 19:28:13 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var slideIndex=0;
+carousel();
+
+function carousel(){
+  var i;
+  var x = document.getElementsByClassName("slide");
+  for(i=0;i<x.length;i++){
+    x[i].style.display = "none";
+  }
+
+  slideIndex++;
+  if(slideIndex > x.length) {slideIndex=1 }
+
+  x[slideIndex-1].style.display = "block";
+
+  setTimeout(carousel,2000);
+
+
+
+}
+
+
+function hamburger() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
